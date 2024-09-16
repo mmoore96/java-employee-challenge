756a2b0c04b57a8cac8f08971ee245cab5038c56
Michael Moore <GMMoore96@outlook.com>
2024-09-16 19:36:55 -0400

    refactor(controller): ♻️ rename IEmployeeController to EmployeeController
    Renamed `IEmployeeController` to `EmployeeController` to align with standard naming conventions, as it's now a concrete class rather than an interface. This change reflects the controller's role more accurately within the application.

--- src/main/java/com/example/rqchallenge/controller/EmployeeController.java
+++ src/main/java/com/example/rqchallenge/controller/EmployeeController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-public interface IEmployeeController {
+public class EmployeeController {
 
     // @GetMapping()
     // ResponseEntity<List<Employee>> getAllEmployees() throws IOException;
