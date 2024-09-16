0d9ab03712ad6c18b20c388a5107f7345878d1ad
Michael Moore <GMMoore96@outlook.com>
2024-09-16 18:53:20 -0400

    fix(controller): 🐛 comment out unimplemented employee controller methods to fix build
    Temporarily commented out unimplemented methods in `IEmployeeController` to prevent build failures. These methods will be implemented in the future, but for now, this change ensures the project can compile successfully.

--- src/main/java/com/example/rqchallenge/employees/IEmployeeController.java
+++ src/main/java/com/example/rqchallenge/employees/IEmployeeController.java
@@ -10,25 +10,25 @@ import java.util.Map;
 @RestController
 public interface IEmployeeController {
 
-    @GetMapping()
-    ResponseEntity<List<Employee>> getAllEmployees() throws IOException;
-
-    @GetMapping("/search/{searchString}")
-    ResponseEntity<List<Employee>> getEmployeesByNameSearch(@PathVariable String searchString);
-
-    @GetMapping("/{id}")
-    ResponseEntity<Employee> getEmployeeById(@PathVariable String id);
-
-    @GetMapping("/highestSalary")
-    ResponseEntity<Integer> getHighestSalaryOfEmployees();
-
-    @GetMapping("/topTenHighestEarningEmployeeNames")
-    ResponseEntity<List<String>> getTopTenHighestEarningEmployeeNames();
-
-    @PostMapping()
-    ResponseEntity<Employee> createEmployee(@RequestBody Map<String, Object> employeeInput);
-
-    @DeleteMapping("/{id}")
-    ResponseEntity<String> deleteEmployeeById(@PathVariable String id);
+    // @GetMapping()
+    // ResponseEntity<List<Employee>> getAllEmployees() throws IOException;
+    //
+    // @GetMapping("/search/{searchString}")
+    // ResponseEntity<List<Employee>> getEmployeesByNameSearch(@PathVariable String searchString);
+    //
+    // @GetMapping("/{id}")
+    // ResponseEntity<Employee> getEmployeeById(@PathVariable String id);
+    //
+    // @GetMapping("/highestSalary")
+    // ResponseEntity<Integer> getHighestSalaryOfEmployees();
+    //
+    // @GetMapping("/topTenHighestEarningEmployeeNames")
+    // ResponseEntity<List<String>> getTopTenHighestEarningEmployeeNames();
+    //
+    // @PostMapping()
+    // ResponseEntity<Employee> createEmployee(@RequestBody Map<String, Object> employeeInput);
+    //
+    // @DeleteMapping("/{id}")
+    // ResponseEntity<String> deleteEmployeeById(@PathVariable String id);
 
 }
